4cd79a4e269d5ecc4bd1eec95cfcf2e7a8538203
unknown <amazighmalek.com>
2025-07-24 16:09:45 +0100

    better-auth

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { Logout } from '@/components/logout'
+import React from 'react'
+
+export default  function Page() {
+  return (
+    <div>
+        dashboard
+  <Logout />
+    </div>
+  )
+}
+
+ 
